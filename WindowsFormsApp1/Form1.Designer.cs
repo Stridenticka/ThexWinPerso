b9955b3572f717194a85ea46a00d84e668c30e1e
Stridenticka <lecontemat@eisti.eu>
2018-06-04 15:04:50 +0900

    first commit on master, connection and disconnebtion ready

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,13 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.disconect = new System.Windows.Forms.Button();
+            this.photo = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 53);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // disconect
+            // 
+            this.disconect.Location = new System.Drawing.Point(190, 10);
+            this.disconect.Name = "disconect";
+            this.disconect.Size = new System.Drawing.Size(156, 53);
+            this.disconect.TabIndex = 1;
+            this.disconect.Text = "disconnect";
+            this.disconect.UseVisualStyleBackColor = true;
+            this.disconect.Click += new System.EventHandler(this.disconect_Click);
+            // 
+            // photo
+            // 
+            this.photo.Location = new System.Drawing.Point(381, 10);
+            this.photo.Name = "photo";
+            this.photo.Size = new System.Drawing.Size(156, 53);
+            this.photo.TabIndex = 2;
+            this.photo.Text = "1 photo";
+            this.photo.UseVisualStyleBackColor = true;
+            this.photo.Click += new System.EventHandler(this.Photo1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(17, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(715, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(299, 115);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(475, 323);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.photo);
+            this.Controls.Add(this.disconect);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button disconect;
+        private System.Windows.Forms.Button photo;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,113 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        private Timer timer1;
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Photo1_Click(object sender, EventArgs e)
+        {
+            //textBox1.Text = "you click on the photo button ";
+            //initialisation timer
+            timer1 = new Timer();
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Interval = 1000; // in miliseconds
+            timer1.Start();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ushort[] imageData = new ushort[288 * 384];
+            byte[] bArr = new byte[288 * 384];
+            Bitmap b = new Bitmap(384, 288, PixelFormat.Format8bppIndexed);
+            ColorPalette ncp = b.Palette;
+            Rectangle BoundsRect = new Rectangle(0, 0, 384, 288);
+            BitmapData bmpData;
+            IntPtr ptr;
+            DLLHelper.RecvImage(imageData, 0);
+            for (int n = 0; n < 288 * 384; n++)
+            {
+                bArr[n] = (byte)(imageData[n] >> 8);
+            }
+            bmpData = b.LockBits(BoundsRect, ImageLockMode.WriteOnly, PixelFormat.Format8bppIndexed);
+            ptr = bmpData.Scan0;
+
+            Marshal.Copy(bArr, 0, ptr, bmpData.Stride * 288);
+            b.UnlockBits(bmpData);
+
+            RefreshImage(b);
+
+
+            if (textBox1.Text.Equals("tick"))
+            {
+                textBox1.Text = "tack";
+            }
+            else
+            {
+                textBox1.Text = "tick";
+            }
+
+        }
+
+        delegate void RefreshImageDelegate(Bitmap bmpData);
+
+        void RefreshImage(Bitmap bmpData)
+        {
+
+            if (pictureBox1.InvokeRequired == false)
+            {
+                pictureBox1.Image = bmpData;
+
+
+
+               // if (btnStop.Enabled == false) btnStop.Enabled = true;
+                //if (btnCalibrate.Enabled == true && liveStream == true) btnCalibrate.Enabled = false;
+            }
+            else
+            {
+                RefreshImageDelegate showProgress = new RefreshImageDelegate(RefreshImage);
+                BeginInvoke(showProgress, new object[] { bmpData });
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IntPtr hWnd = this.Handle;
+
+            int bRes = DLLHelper.UsbOpenDevice(hWnd, 0);
+            if (bRes == 1)
+            {
+                textBox1.Text = "Connected!";
+            }
+            else
+            {
+                textBox1.Text = "Err:" + bRes.ToString();
+            }
+
+            bRes = DLLHelper.ReadFlashData(0);
+            if (bRes == 1)
+            {
+                textBox1.Text = "Flash read completed";
+               //need to enable th good buttons
+                // btnStream.Enabled = true;
+                //btnDisconnect.Enabled = true;
+               // btnCalibrate.Enabled = true;
+            }
+            else
+            {
+                textBox1.Text = "Err:" + bRes.ToString();
+            }
+
+        }
+
+        private void disconect_Click(object sender, EventArgs e)
+        {
+            DLLHelper.UsbCloseDevice(0);
+            textBox1.Text = "Disconected!";
+
+        }
+
+        
     }
 }
